297dd3b30323058fd0e96cd7f11656f3596eecd2
Ania Nunez <ania.nunez@noseryoung.com>
2024-05-05 18:33:32 +0200

    Main und html
    Main und HTML in Anias Projekt überarbeitet

--- Anias Projekt/M122/Filmdatenbank/Main.py
+++ Anias Projekt/M122/Filmdatenbank/Main.py
@@ -1,44 +1,64 @@
+"""from flask import Flask, render_template
 import psycopg2
+app = Flask(__name__)
 
 dbname = "moviedb"
 user = "ania"
 password = "123"
 host = "localhost"
+port = "5432"
+"""
 
-conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+from flask import Flask, render_template
+import psycopg2
 
+app = Flask(__name__)
+
+user = "utopia1"
+password = "060507"
+host = "localhost"
+dbname = "moviedb"
+
+
+conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
 cur = conn.cursor()
 
-select_query = """
+query = """
+INSERT INTO movies (name, release_date, length_minutes, rating, category)
+VALUES (%s, %s, %s, %s, %s);
+"""
+
+check_query = """
 SELECT * FROM movies WHERE name = %s AND release_date = %s;
 """
-
-insert_query = """
-INSERT INTO movies (id, name, release_date, length_minutes, rating, category)
-VALUES (%s, %s, %s, %s, %s, %s);
-"""
-
-def get_next_available_id():
-    cur.execute("SELECT id FROM movies WHERE id BETWEEN 1 AND 10;")
-    ids = {i[0] for i in cur.fetchall()}
-    all_ids = set(range(1, 11))
-    available_ids = all_ids - ids
-    return min(available_ids) if available_ids else None
 
 movie_data = [
     ("Spiderman: No Way Home", "2021-12-17", 148, "8.2", "Action Adventure Fantasy"),
     ("The Matrix Resurrections", "2021-12-22", 148, "5.8", "Action Sci-Fi"),
+    ("Spider-Man: Across the Spider-Verse", "2022-06-02", 140, "8.6", "Action Animation"),
+    ("Iron Man", "2008-04-30", 126, "7.9", "Action"),
+    ("Emily Erdbeer", "2012-12-13", 30, "5.5", "Animation"),
+    ("Murder Mystery", "2019-06-14", 97, "6.0", "Detective Stories"),
+    ("SpongeBob Schwammkopf", "1999-05-01", 23, "8.2", "Comedy Slapstick"),
+    ("One Piece Film: Red", "2022-08-06", 100, "8.4", "Action Adventure Animation")
 ]
 
 for movie in movie_data:
-    cur.execute(select_query, (movie[0], movie[1]))
-    if cur.fetchone() is not None:
-        continue
-    next_id = get_next_available_id()
-    if next_id is None:
-        continue
-    cur.execute(insert_query, (next_id, *movie))
+    cur.execute(check_query, (movie[0], movie[1]))
+    if cur.fetchone() is None:
+        cur.execute(query, movie)
+
 conn.commit()
+
+@app.route('/')
+def home():
+    cur.execute("SELECT * FROM movies;")
+    movies = cur.fetchall()
+    return render_template('index.html', movies=movies)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+
 
 cur.close()
 conn.close()
