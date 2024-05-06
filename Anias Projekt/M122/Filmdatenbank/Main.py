bd92335ffb82fc5ee915b5edd21a796268bc55a2
Ania Nunez <ania.nunez@noseryoung.com>
2024-05-06 14:36:16 +0200

    Um mit Datenbank zu verbinden
    Um mit Datenbank zu verbinden -Anias Projekt

--- Anias Projekt/M122/Filmdatenbank/Main.py
+++ Anias Projekt/M122/Filmdatenbank/Main.py
@@ -1,30 +1,23 @@
-"""from flask import Flask, render_template
+from flask import Flask, render_template
 import psycopg2
 app = Flask(__name__)
 
 dbname = "moviedb"
 user = "ania"
 password = "123"
-host = "localhost"
+host = "192.168.36.138"
 port = "5432"
-"""
 
-from flask import Flask, render_template
-import psycopg2
+try:
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+except psycopg2.OperationalError as e:
+    print(f"Unable to connect to the database: {e}")
+    exit(1)
 
-app = Flask(__name__)
-
-user = "utopia1"
-password = "060507"
-host = "localhost"
-dbname = "moviedb"
-
-
-conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
 cur = conn.cursor()
 
 query = """
-INSERT INTO movies (name, release_date, length_minutes, rating, category)
+INSERT INTO movies (name, release_date, category, length_minutes, rating)
 VALUES (%s, %s, %s, %s, %s);
 """
 
@@ -33,14 +26,12 @@ SELECT * FROM movies WHERE name = %s AND release_date = %s;
 """
 
 movie_data = [
-    ("Spiderman: No Way Home", "2021-12-17", 148, "8.2", "Action Adventure Fantasy"),
-    ("The Matrix Resurrections", "2021-12-22", 148, "5.8", "Action Sci-Fi"),
-    ("Spider-Man: Across the Spider-Verse", "2022-06-02", 140, "8.6", "Action Animation"),
-    ("Iron Man", "2008-04-30", 126, "7.9", "Action"),
-    ("Emily Erdbeer", "2012-12-13", 30, "5.5", "Animation"),
-    ("Murder Mystery", "2019-06-14", 97, "6.0", "Detective Stories"),
-    ("SpongeBob Schwammkopf", "1999-05-01", 23, "8.2", "Comedy Slapstick"),
-    ("One Piece Film: Red", "2022-08-06", 100, "8.4", "Action Adventure Animation")
+    ("Spiderman: No Way Home", "2021-12-17", "Action Adventure Fantasy", 148, "8.2"),
+    ("The Matrix Resurrections", "2021-12-22", "Action Sci-Fi", 148, "5.8"),
+    ("Spider-Man: Across the Spider-Verse", "2022-06-02", "Action Animation", 140, "8.6"),
+    ("Iron Man", "2008-04-30", "Action", 126, "7.9", ),
+    ("Emily Erdbeer", "2012-12-13", "Animation", 30, "5.5"),
+    ("Murder Mystery", "2019-06-14", "Detective Stories", 97, "6.0"),
 ]
 
 for movie in movie_data:
@@ -57,8 +48,7 @@ def home():
     return render_template('index.html', movies=movies)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5432)
 
 cur.close()
 conn.close()
